590f9bcbe30a26b9f04eaf83f5ef859f0393255c
Zara <zara@coit.es>
2012-02-13 13:42:00 +0100

    GCC builder more corrections

--- build-gcc-multiarch.sh
+++ build-gcc-multiarch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 
-GCC_BASE_DIR?=~/curro/gcc
-GCC_SOURCE_DIR?=$GCC_BASE_DIR/gcc
-GCC_OBJECT_DIR?=$GCC_BASE_DIR/gcc-obj
-SUFFIX?=-test
+GCC_BASE_DIR=~/curro/gcc
+GCC_SOURCE_DIR=$GCC_BASE_DIR/gcc
+GCC_OBJECT_DIR=$GCC_BASE_DIR/gcc-obj
+SUFFIX=-test
 ARCH_NAME=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
 
-rm -rf &GCC_OBJECT_DIR
-mkdir -p &GCC_OBJECT_DIR
-pushd &GCC_OBJECT_DIR
+rm -rf $GCC_OBJECT_DIR
+mkdir -p $GCC_OBJECT_DIR
+pushd $GCC_OBJECT_DIR
 
 
 $GCC_SOURCE_DIR/configure --program-suffix=$SUFFIX --enable-languages=c,c++ --enable-targets=all CFLAGS_FOR_TARGET="-g -O2 -B/usr/lib/x86_64-linux-gnu"
