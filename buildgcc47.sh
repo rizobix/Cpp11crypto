b1e87a695ffa3eeea9b1b4fabf6247b3e7b0138e
Zara <zara@coit.es>
2012-02-13 09:32:32 +0100

    raw version of scritp to compile gcc-4.7 on Debian/multiarch, wheezy and sid. Probably works also on other multiarch, as Ubuntu

---
+++ buildgcc47.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+GCC_BASE_DIR?=~/curro/gcc
+GCC_SOURCE_DIR?=$GCC_BASE_DIR/gcc
+GCC_OBJECT_DIR?=$GCC_BASE_DIR/gcc-obj
+
+rm -rf &GCC_OBJECT_DIR
+mkdir -p &GCC_OBJECT_DIR
+pushd &GCC_OBJECT_DIR
+
+
+$GCC_SOURCE_DIR/configure --program-suffix=test --enable-languages=c,c++ --enable-targets=all CFLAGS_FOR_TARGET="-g -O2 -B/usr/lib/x86_64-linux-gnu"
+
+make -j 4 BOOT_CFLAGS="-g -O2 -B/usr/lib/x86_64-linux-gnu"
+
+popd
