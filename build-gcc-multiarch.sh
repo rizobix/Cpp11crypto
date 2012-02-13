19a7004c2f4e33c9a12fd6e2d85790cb9719968f
Zara <zara@coit.es>
2012-02-13 11:14:24 +0100

    GCC builder seems OK

--- build-gcc-multiarch.sh
+++ build-gcc-multiarch.sh
@@ -3,14 +3,17 @@
 GCC_BASE_DIR?=~/curro/gcc
 GCC_SOURCE_DIR?=$GCC_BASE_DIR/gcc
 GCC_OBJECT_DIR?=$GCC_BASE_DIR/gcc-obj
+SUFFIX?=-test
+ARCH_NAME=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
 
 rm -rf &GCC_OBJECT_DIR
 mkdir -p &GCC_OBJECT_DIR
 pushd &GCC_OBJECT_DIR
 
 
-$GCC_SOURCE_DIR/configure --program-suffix=test --enable-languages=c,c++ --enable-targets=all CFLAGS_FOR_TARGET="-g -O2 -B/usr/lib/x86_64-linux-gnu"
+$GCC_SOURCE_DIR/configure --program-suffix=$SUFFIX --enable-languages=c,c++ --enable-targets=all CFLAGS_FOR_TARGET="-g -O2 -B/usr/lib/x86_64-linux-gnu"
 
 make -j 4 BOOT_CFLAGS="-g -O2 -B/usr/lib/x86_64-linux-gnu"
+sudo make -j 4 install-strip
 
 popd
