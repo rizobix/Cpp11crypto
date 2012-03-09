e669f5553171f6eeb4768e97cceb0dee9f55d74b
Zara <zara@coit.es>
2012-03-09 07:51:30 +0100

    This build gile for gcc 4.7 may work

--- build-gcc-multiarch.sh
+++ build-gcc-multiarch.sh
@@ -6,17 +6,17 @@ GCC_OBJECT_DIR=$GCC_BASE_DIR/gcc-obj
 SUFFIX=-test
 ARCH_NAME=$(dpkg-architecture -qDEB_HOST_MULTIARCH)
 NUM_PROCESSES=4
+MULTIFLAGS="-g -O2 -B/usr/lib/x86_64-linux-gnu"
+# -I/usr/include/$ARCH_NAME"
 
 rm -rf $GCC_OBJECT_DIR
 mkdir -p $GCC_OBJECT_DIR
 pushd $GCC_OBJECT_DIR
 
 
-$GCC_SOURCE_DIR/configure --program-suffix=$SUFFIX --enable-languages=c,c++ --enable-targets=all CFLAGS_FOR_TARGET="-g -O2 -B/usr/lib/$ARCH_NAME"
-# -I/usr/inlcude/$ARCH_NAME"
+$GCC_SOURCE_DIR/configure --program-suffix=$SUFFIX --enable-languages=c,c++ --enable-targets=all CFLAGS_FOR_TARGET="$MULTIFLAGS"
 
-make -j $NUM_PROCESSES BOOT_CFLAGS="-g -O2 -B/usr/lib/$ARCH_NAME"
-# -I/usr/inlcude/$ARCH_NAME" XGCC_FLAGS_FOR_TARGET="-g -O2 -B/usr/lib/$ARCH_NAME -I/usr/inlcude/$ARCH_NAME"
+make -j $NUM_PROCESSES BOOT_CFLAGS="$MULTIFLAGS"
 sudo make -j $NUM_PROCESSES install-strip
 
 popd
